acbc9ce197597c52161c40f5d7d3592525f1fddb
Codenames-Ren <renmizuki38@gmail.com>
2025-05-06 19:39:20 +0700

    Menambahkan file dosen, matakuliah, dan mahasiswa

---
+++ bayumahasiswa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php';?>
+  </head>
+  <body>
+    <header>
+      <h2>Data Mahasiswa</h2>
+    </header>
+    <section>
+    <?php 
+    include 'navbar.php';
+    include 'bayumahasiswatampil.php';
+    ?>
+    </section>
+    <?php include 'footer.php';?>
+  </body>
+</html>
