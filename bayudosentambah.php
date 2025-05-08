f8528a2808bf8cfc4f0257da29c5c98e62a78c52
Codenames-Ren <renmizuki38@gmail.com>
2025-05-08 18:40:24 +0700

    Menambah file tambah data dosen. Sekarang input data dosen sudah berada dalam 1 frame

---
+++ bayudosentambah.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php';?>
+  <body>
+    <header>
+      <h2>Data Dosen</h2>
+    </header>
+    <section>
+    <?php 
+    include 'navbar.php';
+    include 'bayudoseninput.php';
+    ?>
+    </section>
+   <?php include 'footer.php';?>
+  </body>
+</html>
